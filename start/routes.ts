6f0413b462694cc1e7cdf81a7e076ada9f5f73f1
Felipeds-L <84592591+Felipeds-L@users.noreply.github.com>
2021-12-13 14:14:01 -0300

    feat: adding new features

--- start/routes.ts
+++ start/routes.ts
@@ -1,35 +1,10 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
-import User from 'App/Models/User'
-import Hash from '@ioc:Adonis/Core/Hash'
 import Route from '@ioc:Adonis/Core/Route'
-import Database from '@ioc:Adonis/Lucid/Database'
+import 'App/Modules/User/routes'
 
-Route.get('/', async () => {
-  return { hello: 'Hello there!s' }
-})
+Route.group(() => {
+  Route.get('/', async () => {
+    return { hello: 'Hello there!s' }
+  })
 
-Route.get('/users', async() => {
-  return Database.from('users').select('*')
-})
-
-Route.post('login', 'AuthController.login')
-
+  Route.post('login', 'AuthController.login')
+}).prefix('/api')
