07cbd5af0fc756157d5c80463a7e4e30c4ff1e55
Felipeds-L <84592591+Felipeds-L@users.noreply.github.com>
2022-03-08 22:11:30 -0300

    feat: x

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,7 +2,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index({  }: HttpContextContract) {
+  public async index({ }: HttpContextContract) {
     const user = await User.all()
 
     return user;
